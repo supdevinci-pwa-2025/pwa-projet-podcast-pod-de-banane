834f1f4053b288582a401e4299b1cd108bfe2aff
Erika <ambinintsoaerika@gmail.com>
2025-07-01 10:26:31 +0200

    ajout cache dans service worker

--- serviceWorker.js
+++ serviceWorker.js
@@ -8,3 +8,19 @@ self.addEventListener('install', event => { // indice: quand le SW est installé
     console.log(' Service Worker activé');
     self.clients.claim( ); // indice: prendre le contrôle des pages ouvertes
   });
+
+  const staticCacheName = "pod-banane-cache";
+  const assets = [
+    "./",
+    "./index.html",
+    "./open.html",
+    "./index.html",
+    "./share.html",
+    "./Dashboard.html",
+    "./public/404.html",
+    "./public/index.html",
+    "./app.js",
+    "./serviceWorker.js",
+    "./functions/share.js",
+    "./style.css",
+  ];
