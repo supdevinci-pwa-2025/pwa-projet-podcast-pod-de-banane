cf2126709180bb5191518161516fbcfbc955a364
Erika <ambinintsoaerika@gmail.com>
2025-07-01 10:38:08 +0200

    ajout cache service worker

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,15 +1,4 @@
-self.addEventListener('install', event => { // indice: quand le SW est installé
-    console.log(' Service Worker installé');
-    self.skipWaiting( ); // indice: forcer à prendre le contrôle immédiatement
-  });
-   
-  // <!-- Écouter l'activation du SW -->
-  self.addEventListener('activate', event => { // indice: quand le SW devient actif
-    console.log(' Service Worker activé');
-    self.clients.claim( ); // indice: prendre le contrôle des pages ouvertes
-  });
-
-  const staticCacheName = "pod-banane-v1";
+const staticCacheName = "pod-banane-v1";
 
   const assets = [
     "./",
@@ -26,3 +15,34 @@ self.addEventListener('install', event => { // indice: quand le SW est installé
     "./assets/manifest-icon-192.maskable.png",
     "./assets/manifest-icon-512.maskable.png"
   ];
+
+  // <!-- INSTALLATION -->
+self.addEventListener('install', event => { // indice: quand le SW est installé
+    console.log(' Service Worker installé');
+    
+    event.waitUntil(
+        caches.open(staticCacheName)
+        .then(cache => cache.addAll(assets))
+        .catch((err) => console.error("Erreur cache install", err))
+    );
+
+    self.skipWaiting( ); // indice: forcer à prendre le contrôle immédiatement
+  });
+   
+  // <!-- ACTIVATE -->
+  self.addEventListener('activate', event => { // indice: quand le SW devient actif
+    console.log(' Service Worker activé');
+
+    event.waitUntil(
+        caches.keys().then(keys => 
+            Promise.all(
+                keys.filter(k => k !== staticCacheName).map(k => caches.delete(k))
+            )
+        )
+    );
+
+    self.clients.claim( ); // indice: prendre le contrôle des pages ouvertes
+  });
+
+  
+
