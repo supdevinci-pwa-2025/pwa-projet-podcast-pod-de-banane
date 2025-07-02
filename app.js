b41d894786c42effea3dd1d96f9efc08739cbcab
Enzo Archambaud <bib.enzoarchambaud@gmail.com>
2025-07-02 13:49:43 +0200

    fix console error 1

--- app.js
+++ app.js
@@ -291,7 +291,7 @@ function setupServiceWorkerListener() {
 async function loadPodcasts() {
   try {
     // Essayer de charger depuis l'API
-    const response = await fetch('https://pod-de-banane.web.app/functions/members');
+    const response = await fetch('/api/pod-banane');
     
     if (response.ok) {
       const data = await response.json();
